186bf9be0e6136f991f740a1f5f53d9b5141e9fe
Pranav Bisaria <pranavbisariya29@gmail.com>
2022-10-05 19:03:55 +0530

    Background Styled, Js Stars in background Added

--- JavaScript/Script.js
+++ JavaScript/Script.js
@@ -27,4 +27,27 @@ function animate(){
 }
 animate();
 
+function points()
+{
+    var a=0;
+    var b=0;
+    
+    for(var n=0; n<250; n++){
+        
+        var st=document.getElementById("bgstars");
+        let circle=document.createElementNS("http://www.w3.org/2000/svg", "circle");
+
+        a=Math.floor(Math.random()*window.innerWidth);
+        b=Math.floor(Math.random()*window.innerHeight);
+        
+        circle.setAttribute("cx", a);
+        circle.setAttribute("cy", b);
+        circle.setAttribute("r",  1);
+        circle.setAttribute("fill", "white");
+        st.appendChild(circle);
+        
+    }
+    
+}
+
 setTimeout(points, 100);
