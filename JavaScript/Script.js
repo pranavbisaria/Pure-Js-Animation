61084c4509f521c37b8b05d361e45d0df0d9e397
Pranav Bisaria <pranavbisariya29@gmail.com>
2022-10-05 12:00:11 +0530

    Background Adjusted and Clouds Added in the Background

--- JavaScript/Script.js
+++ JavaScript/Script.js
@@ -1,12 +1,11 @@
 setTimeout(points, 100);
-setTimeout(background, 100);
 
 function points()
 {
     var a=0;
     var b=0;
 
-    for(var n=0; n<10; n++){
+    for(var n=0; n<250; n++){
         
         var st=document.getElementById("bgstars");
         let circle=document.createElementNS("http://www.w3.org/2000/svg", "circle");
@@ -21,34 +20,4 @@ function points()
         st.appendChild(circle);
 
     }
-}
-
-
-
-function background()
-{
-
-    let number=0;
-    setInterval(appear, 40);
-    function appear()
-    {
-
-        const str=document.getElementById("bgstars");
-        var rect=str.getBoundingClientRect();
-        var x=rect.top;
-        x++;
-        document.getElementById("bgstars").style.top=x;
-        
-        if(number<=20){
-            points();
-        }
-
-        if(x==100)
-        {
-            x--;
-        }
-
-        number++;
-    }
-
 }
